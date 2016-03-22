7ff8a37c1189ff7e763a6093eb4913a1867499be
Astha Sharma <a@astha.me>
2016-03-22 14:39:00 +0530

    Implemented the chat effect but with a bug

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
-$(window).scroll(function() {
-	if($(this).scrollTop() > 1){
-		$('ul, img.logo, .top').addClass("sticky");
-	}
-	else {
-		$('ul, img.logo, .top').removeClass("sticky");
-	}
-});
+function initWow() {
+  new WOW().init();
+}
+
+function setUpScroll() {
+  $(window).scroll(function() {
+    if($(this).scrollTop() > 1){
+      $('ul, img.logo, .top').addClass("sticky");
+    }
+    else {
+      $('ul, img.logo, .top').removeClass("sticky");
+    }
+  });
+}
+
+$(document).ready(function(){
+
+  initWow();
+  setUpScroll();
+
+  $('.typing').each(function(i){
+    var counter = 200 + i * 1000;
+
+    var selector = $(this);
+    var time = parseInt(selector.siblings().attr('data-wow-delay'));
+    var toSeconds = (time * 1000) - 100;
+
+    var showTyping = setInterval(function() {
+      selector.fadeIn();
+    }, counter);
+
+    var hideTyping = setInterval(function() {
+      clearInterval(showTyping);
+      selector.fadeOut();
+    }, toSeconds);
+
+    console.log(toSeconds);
+  });
+})
