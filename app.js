1b4805e6cbaa375b616ccb20033064cb0c6afba4
Astha Sharma <a@astha.me>
2016-03-22 18:51:53 +0530

    Implemented automatic scrolling with a bug

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ $(document).ready(function(){
 
   initWow();
   setUpScroll();
+  setTimeout(function(){
+    $("#chat").animate({ scrollTop: 700 }, 50000);
+  }, 3000)
 
   $('.typing1').each(function(i){
     console.log(i);
@@ -30,6 +33,50 @@ $(document).ready(function(){
     console.log(toSeconds);
   });
   $('.typing2').each(function(i){
+    console.log(i);
+    var selector = $(this);
+    var time = parseFloat(selector.siblings().attr('data-wow-delay'));
+    var toSeconds = (time * 1000) - 300;
+    var hideTyping = setInterval(function() {
+      selector.fadeOut();
+    }, toSeconds);
+
+    console.log(toSeconds);
+  });
+  $('.typing3').each(function(i){
+    console.log(i);
+    var selector = $(this);
+    var time = parseFloat(selector.siblings().attr('data-wow-delay'));
+    var toSeconds = (time * 1000) - 300;
+    var hideTyping = setInterval(function() {
+      selector.fadeOut();
+    }, toSeconds);
+
+    console.log(toSeconds);
+  });
+  $('.typing4').each(function(i){
+    console.log(i);
+    var selector = $(this);
+    var time = parseFloat(selector.siblings().attr('data-wow-delay'));
+    var toSeconds = (time * 1000) - 300;
+    var hideTyping = setInterval(function() {
+      selector.fadeOut();
+    }, toSeconds);
+
+    console.log(toSeconds);
+  });
+  $('.typing5').each(function(i){
+    console.log(i);
+    var selector = $(this);
+    var time = parseFloat(selector.siblings().attr('data-wow-delay'));
+    var toSeconds = (time * 1000) - 300;
+    var hideTyping = setInterval(function() {
+      selector.fadeOut();
+    }, toSeconds);
+
+    console.log(toSeconds);
+  });
+  $('.typing6').each(function(i){
     console.log(i);
     var selector = $(this);
     var time = parseFloat(selector.siblings().attr('data-wow-delay'));
